35c9b0f9b0b08b0be27a769ae016a5c05e2d4c42
Saquiba Wasi <115282275+SAQUIBAWASI@users.noreply.github.com>
2022-12-12 19:25:09 +0530

    Add files via upload

---
+++ Day_11_1_Reverse Digits.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int reverseDigits(int num)
+{
+    int rev_num = 0;
+    while (num > 0) 
+    {
+        rev_num = rev_num * 10 + num % 10;
+        num = num / 10;
+    }
+    return rev_num;
+}
+int main()
+{
+    int num ;
+    printf("Enter the number:");
+    scanf("%d",&num);
+    printf("Reverse of no. is %d", reverseDigits(num));
+    return 0;
+}
