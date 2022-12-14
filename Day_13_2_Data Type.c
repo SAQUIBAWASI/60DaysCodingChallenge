2c861ad461937afa0e9f1cc63dcbb9059def8ecc
Saquiba Wasi <115282275+SAQUIBAWASI@users.noreply.github.com>
2022-12-14 21:15:07 +0530

    Add files via upload

---
+++ Day_13_2_Data Type.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int integerType;
+    char charType;
+    float floatType;
+    double doubleType;
+   printf("Size of int is: %ld
+",
+           sizeof(integerType));
+  
+     printf("Size of char is: %ld
+",
+           sizeof(charType));
+  
+     printf("Size of float is: %ld
+",
+           sizeof(floatType));
+  
+    printf("Size of double is: %ld
+",
+           sizeof(doubleType));
+  
+    return 0;
+}
