f3045eb032acdf9de3063df26527c592e963837f
Saquiba Wasi <115282275+SAQUIBAWASI@users.noreply.github.com>
2022-12-21 19:48:33 +0530

    Add files via upload

---
+++ Day_20_1 String using scanf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[50];
+    printf("Enter any string: ");
+    scanf("%s",str);
+    printf("You have entered: %s",str);
+    return 0;   
+}
+
---
+++ Day_20_2 String using get.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[50];
+    printf("\nEnter any sentence: ");
+    gets(str);
+    printf("\nYou have entered: ");
+    puts(str);
+    return 0;
+    
+}
